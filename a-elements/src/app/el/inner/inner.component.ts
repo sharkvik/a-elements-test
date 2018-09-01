f3755f030ccce513084166e78311ab65ac588d8a
Виктор Наприенко <shark_vik@inbox.ru>
2018-09-01 17:53:45 +0300

    ...

---
+++ a-elements/src/app/el/inner/inner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nvm-inner',
+  templateUrl: './inner.component.html',
+  styleUrls: ['./inner.component.scss']
+})
+export class InnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
